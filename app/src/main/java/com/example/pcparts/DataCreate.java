e8672cb10f92f773fd25501872989ae32a5451fb
Wojciech Łoś <wojtek.los2003@gmail.com>
2023-05-16 15:00:12 +0200

    wyswietlanie w przegladarce, dodate funkcjedatacreate

--- app/src/main/java/com/example/pcparts/DataCreate.java
+++ app/src/main/java/com/example/pcparts/DataCreate.java
@@ -8,14 +8,24 @@ import java.util.List;
 public class DataCreate {
     public static List<Integer> imges;
     public static String name;
-    DataCreate(){
-        imges= Arrays.asList();
-        name="";
+    public static String price;
+    public static String webLink;
+    public static int table;
+
+    DataCreate() {
+        imges = Arrays.asList();
+        name = "";
+        price="";
+        webLink="";
+        table = 0;
+
     }
 
-    public DataCreate(List<Integer> imges,String name){
-        this.name=name;
-        this.imges=imges;
-
+    public DataCreate(List<Integer> imges, String name, int table,String price,String webLink) {
+        this.name = name;
+        this.imges = imges;
+        this.table = table;
+        this.price=price;
+        this.webLink=webLink;
     }
 }
--- app/src/main/java/com/example/pcparts/SecondActivity.java
+++ app/src/main/java/com/example/pcparts/SecondActivity.java
@@ -1,10 +1,11 @@
 package com.example.pcparts;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,8 +13,6 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 import java.util.List;
 
@@ -23,6 +22,9 @@ public class SecondActivity extends AppCompatActivity  {
     Button BT1;
     Button BT3;
     TextView TVnazwa;
+    ImageView IMGV1;
+    TextView PRICE;
+
 
 
 
@@ -30,13 +32,20 @@ public class SecondActivity extends AppCompatActivity  {
     private DataCreate getImageListForButton(int buttonIndex) {
         List<Integer> imges=Arrays.asList();
         String name="";
-        DataCreate data = new DataCreate(imges,name);
+        String price="";
+        String webLink="";
+        int table=0;
+
+        DataCreate data = new DataCreate(imges,name,table,price,webLink);
 
         // Determine the appropriate imageList based on the buttonIndex
         switch (buttonIndex) {
             case 1:
                 data.imges = Arrays.asList(R.drawable.image1, R.drawable.image2);
-                data.name = "Nazzwa1";
+                data.name = "Intel i9";
+                data.table = R.drawable.image2;
+                data.price = "1459 zł";
+                data.webLink="https://www.x-kom.pl/p/533441-procesor-intel-core-i9-intel-core-i9-10980xe.html";
                 break;
 //            case 2:
 //                imageList = Arrays.asList(R.drawable.image4, R.drawable.image5, R.drawable.image6);
@@ -58,6 +67,8 @@ public class SecondActivity extends AppCompatActivity  {
         BT1= findViewById(R.id.BT1);
         BT3= findViewById(R.id.BT3);
         TVnazwa=findViewById(R.id.TVNazwa);
+        IMGV1=findViewById(R.id.IMGV1);
+        PRICE=findViewById(R.id.PIRCE);
         BT1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,6 +77,7 @@ public class SecondActivity extends AppCompatActivity  {
             }
         });
 
+
         Bundle extras = getIntent().getExtras();
         if (extras != null && extras.containsKey("buttonIndex")) {
             int buttonIndex = extras.getInt("buttonIndex");
@@ -73,8 +85,18 @@ public class SecondActivity extends AppCompatActivity  {
             ViewPager2 viewPager2 = findViewById(R.id.VP0);
             List<Integer> imageList = getImageListForButton(buttonIndex).imges;
             TVnazwa.setText(getImageListForButton(buttonIndex).name);
+            IMGV1.setImageResource(getImageListForButton(buttonIndex).table);
+            PRICE.setText(getImageListForButton(buttonIndex).price);
             ImagePagerAdapter imagePagerAdapter = new ImagePagerAdapter(imageList);
             viewPager2.setAdapter(imagePagerAdapter);//
+            BT3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url =getImageListForButton(buttonIndex).webLink;
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
         }
     }
 }
