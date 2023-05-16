8308d587eeb5f870d6a0ff012c0294f3fa44aa56
Wojciech Łoś <wojtek.los2003@gmail.com>
2023-05-16 13:25:53 +0200

    dziala wyswietlanie z case-ow

--- app/src/main/java/com/example/pcparts/MainActivity.java
+++ app/src/main/java/com/example/pcparts/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,6 +21,56 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity{
 
     ImageButton IB1;
+    ImageButton IB2;
+    ImageButton IB3;
+    ImageButton IB4;
+    ImageButton IB5;
+    ImageButton IB6;
+    ImageButton IB7;
+    ImageButton IB8;
+    ImageButton IB9;
+    ImageButton IB10;
+    ImageButton IB11;
+    ImageButton IB12;
+    ImageButton IB13;
+    ImageButton IB14;
+    ImageButton IB15;
+    ImageButton IB16;
+    ImageButton IB17;
+    ImageButton IB18;
+    ImageButton IB19;
+    ImageButton IB20;
+    ImageButton IB21;
+    ImageButton IB22;
+    ImageButton IB23;
+    ImageButton IB24;
+
+
+    TextView TV1;
+    TextView TV2;
+    TextView TV3;
+    TextView TV4;
+    TextView TV5;
+    TextView TV6;
+    TextView TV7;
+    TextView TV8;
+    TextView TV9;
+    TextView TV10;
+    TextView TV11;
+    TextView TV12;
+    TextView TV13;
+    TextView TV14;
+    TextView TV15;
+    TextView TV16;
+    TextView TV17;
+    TextView TV18;
+    TextView TV19;
+    TextView TV20;
+    TextView TV21;
+    TextView TV22;
+    TextView TV23;
+    TextView TV24;
+
 
 
 
@@ -28,10 +79,60 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         IB1 = findViewById(R.id.IB1);
+        IB2 = findViewById(R.id.IB2);
+        IB3 = findViewById(R.id.IB3);
+        IB4 = findViewById(R.id.IB4);
+        IB5 = findViewById(R.id.IB5);
+        IB6 = findViewById(R.id.IB6);
+        IB7 = findViewById(R.id.IB7);
+        IB8 = findViewById(R.id.IB8);
+        IB9 = findViewById(R.id.IB9);
+        IB10 = findViewById(R.id.IB10);
+        IB11 = findViewById(R.id.IB11);
+        IB12 = findViewById(R.id.IB12);
+        IB13 = findViewById(R.id.IB13);
+        IB14 = findViewById(R.id.IB14);
+        IB15 = findViewById(R.id.IB15);
+        IB16 = findViewById(R.id.IB16);
+        IB17 = findViewById(R.id.IB17);
+        IB18 = findViewById(R.id.IB18);
+        IB19 = findViewById(R.id.IB19);
+        IB20 = findViewById(R.id.IB20);
+        IB21 = findViewById(R.id.IB21);
+        IB22 = findViewById(R.id.IB22);
+        IB23 = findViewById(R.id.IB23);
+        IB24 = findViewById(R.id.IB24);
+
+        TV1 = findViewById(R.id.TV1);
+        TV2 = findViewById(R.id.TV2);
+        TV3 = findViewById(R.id.TV3);
+        TV4 = findViewById(R.id.TV4);
+        TV5 = findViewById(R.id.TV5);
+        TV6 = findViewById(R.id.TV6);
+        TV7 = findViewById(R.id.TV7);
+        TV8 = findViewById(R.id.TV8);
+        TV9 = findViewById(R.id.TV9);
+        TV10 = findViewById(R.id.TV10);
+        TV11 = findViewById(R.id.TV11);
+        TV12 = findViewById(R.id.TV12);
+        TV13 = findViewById(R.id.TV13);
+        TV14 = findViewById(R.id.TV14);
+        TV15 = findViewById(R.id.TV15);
+        TV16 = findViewById(R.id.TV16);
+        TV17 = findViewById(R.id.TV17);
+        TV18 = findViewById(R.id.TV18);
+        TV19 = findViewById(R.id.TV19);
+        TV20 = findViewById(R.id.TV20);
+        TV21 = findViewById(R.id.TV21);
+        TV22 = findViewById(R.id.TV22);
+        TV23 = findViewById(R.id.TV23);
+        TV24 = findViewById(R.id.TV24);
+
         IB1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra("buttonIndex", 1);
                 startActivity(intent);
             }
         });//
--- app/src/main/java/com/example/pcparts/SecondActivity.java
+++ app/src/main/java/com/example/pcparts/SecondActivity.java
@@ -6,18 +6,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
 
+
+    private List<Integer> getImageListForButton(int buttonIndex) {
+        List<Integer> imageList;
+
+        // Determine the appropriate imageList based on the buttonIndex
+        switch (buttonIndex) {
+            case 1:
+                imageList = Arrays.asList(R.drawable.image1, R.drawable.image2);
+                break;
+//            case 2:
+//                imageList = Arrays.asList(R.drawable.image4, R.drawable.image5, R.drawable.image6);
+//                break;
+            // Add more cases as needed
+            default:
+                imageList = Collections.emptyList();
+                break;
+        }
+
+        return imageList;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        ViewPager2 viewPager2 = findViewById(R.id.VP0);
-        List<Integer> imageList = Arrays.asList(R.drawable.image1, R.drawable.image2, R.drawable.image3);
-        ImagePagerAdapter imagePagerAdapter = new ImagePagerAdapter(imageList);
-        viewPager2.setAdapter(imagePagerAdapter);//
+        Bundle extras = getIntent().getExtras();
+        if (extras != null && extras.containsKey("buttonIndex")) {
+            int buttonIndex = extras.getInt("buttonIndex");
+
+            ViewPager2 viewPager2 = findViewById(R.id.VP0);
+            List<Integer> imageList = getImageListForButton(buttonIndex);
+            ImagePagerAdapter imagePagerAdapter = new ImagePagerAdapter(imageList);
+            viewPager2.setAdapter(imagePagerAdapter);//
+        }
     }
 }
